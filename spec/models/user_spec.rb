571d169d3d734a03bb7c29e9f8178c4d09163854
lfga98 <lfgarciauaz@gmail.com>
2018-07-16 15:22:37 -0500

    Added number validations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,12 +3,13 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   describe "creation" do
     before do
-      @user = User.create(email: "test@test.com",password: "asdfasdf",password_confirmation: "asdfasdf", first_name: "Luis", last_name: "Garcia", maternal_surname: "Acosta")
+      @user = User.create(email: "test@test.com",password: "asdfasdf",password_confirmation: "asdfasdf", first_name: "Luis", last_name: "Garcia", maternal_surname: "Acosta",number:"1234567890")
     end
 
     it "can be created" do
       expect(@user).to be_valid
     end
+
     it "has to contain a first name" do
       @user.first_name=nil
       expect(@user).to_not be_valid
@@ -19,6 +20,20 @@ RSpec.describe User, type: :model do
     end
     it "has to contain a martenal surname" do
       @user.maternal_surname=nil
+      expect(@user).to_not be_valid
+    end
+    it "has to contain a telephone number" do
+      @user.number=nil
+      expect(@user).to_not be_valid
+    end
+
+    it "phone number has to be a number" do
+      @user.number="abcdefghij"
+      expect(@user).to_not be_valid
+    end
+
+    it "phone number has to be a 10 digits" do
+      @user.number="12345678901"
       expect(@user).to_not be_valid
     end
 
