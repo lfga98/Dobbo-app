ca41ac2beb07b32a7108b4cc889cd3bee20ea1b5
lfga98 <lfgarciauaz@gmail.com>
2018-07-16 14:52:23 -0500

    Added rspec and validations for first_name last_name and materna_surname

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "creation" do
+    before do
+      @user = User.create(email: "test@test.com",password: "asdfasdf",password_confirmation: "asdfasdf", first_name: "Luis", last_name: "Garcia", maternal_surname: "Acosta")
+    end
+
+    it "can be created" do
+      expect(@user).to be_valid
+    end
+    it "has to contain a first name" do
+      @user.first_name=nil
+      expect(@user).to_not be_valid
+    end
+    it "has to contain a last name" do
+      @user.last_name=nil
+      expect(@user).to_not be_valid
+    end
+    it "has to contain a martenal surname" do
+      @user.maternal_surname=nil
+      expect(@user).to_not be_valid
+    end
+  end
 end
