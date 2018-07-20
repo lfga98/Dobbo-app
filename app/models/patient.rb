d45b833fbd926913bc122b1ac2af50e2cf7dabc3
lfga98 <lfgarciauaz@gmail.com>
2018-07-20 15:25:45 -0500

    Added validations

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,4 +1,4 @@
 class Patient < ApplicationRecord
   belongs_to :user,optional: true
-
+  validates_presence_of :first_name,:last_name,:age,:date_birth,:maternal_surname
 end
--- spec/models/patient_spec.rb
+++ spec/models/patient_spec.rb
@@ -2,16 +2,36 @@ require 'rails_helper'
 
 RSpec.describe Patient, type: :model do
 
-  describe "creation" do
-    before do
-      @patient = Patient.create(first_name:"Leonardo",last_name: "Garcia", maternal_surname: "Acosta",age:16,date_birth: Date.today)
-    end
-    it "can be created" do
-      expect(@patient).to be_valid
-    end
-
-    
+  let(:patient) do
+    Patient.create(first_name:"Leonardo",last_name: "Garcia", maternal_surname: "Acosta",age:16,date_birth: Date.today)
   end
 
+  describe "creation" do
+    before do
+
+    end
+    it "can be created" do
+      expect(patient).to be_valid
+    end
+  end
+
+  describe "validations" do
+    it "has to have a first name" do
+      patient.first_name = nil
+      expect(patient).to_not be_valid
+    end
+    it "has to have a maternal surname" do
+      patient.maternal_surname = nil
+      expect(patient).to_not be_valid
+    end
+    it "has to have an age " do
+      patient.age = nil
+      expect(patient).to_not be_valid
+    end
+    it "has to have a date of birth" do
+      patient.date_birth = nil
+      expect(patient).to_not be_valid
+    end
+  end
 
 end
