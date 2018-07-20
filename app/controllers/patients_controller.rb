5d02595bdb476aed0a731721abaf4f5aa4e88336
lfga98 <lfgarciauaz@gmail.com>
2018-07-19 19:53:44 -0500

    Added a form and create and new methods in the controller

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,6 +1,25 @@
 class PatientsController < ApplicationController
   def index
+    @patients = Patient.all
+  end
+
+  def new
+    @patient = Patient.new
+  end
+
+  def create
+    @patient= Patient.new(patient_params)
+    @patient.save
+
 
   end
+
+  private
+
+  def patient_params
+    params.require(:patient).permit(:first_name,:last_name,:maternal_surname,:age,:date_birth)
+  end
+
+
 
 end
--- spec/features/patient_spec.rb
+++ spec/features/patient_spec.rb
@@ -5,6 +5,8 @@ describe 'navigate' do
   before do
     user = User.create(email: "test@test.com",password: "asdfasdf",password_confirmation: "asdfasdf", first_name: "Luis", last_name: "Garcia", maternal_surname: "Acosta",telephone:"1234567890")
     login_as(user, :scope => :user)
+
+    @patient=Patient.create(first_name:"Leonardo",last_name: "Garcia", maternal_surname: "Acosta",age:10,date_birth: Date.today)
   end
 
   describe 'index' do
@@ -12,15 +14,37 @@ describe 'navigate' do
       visit patients_path
       expect(page).to have_content("Informacion")
     end
-
     it 'has de information of the patient' do
       visit patients_path
-      expect(page).to have_content("Leonardo")
-    end
-    it 'a patient can be created from a page' do
-      visit new_patient_path
-      expect(page.status_code).to eq(200)
+      expect(page).to have_content(@patient.first_name)
     end
 
   end
+
+  describe 'creation' do
+    before do
+      visit new_patient_path
+    end
+
+    it 'has a new page for creation' do
+      expect(page.status_code).to eq(200)
+    end
+
+    it 'has a creation form' do
+      fill_in 'patient[first_name]', with:  'Jacqueline'
+      fill_in 'patient[last_name]', with:  'Garcia'
+      fill_in 'patient[maternal_surname]', with:  'Acosta'
+      fill_in 'patient[age]', with: 12
+      fill_in 'patient[date_birth]', with: Date.today
+
+      expect { click_on "Guardar" }.to change(Patient, :count).by(1)
+    end
+
+
+
+  end
+
+
+
+
 end
