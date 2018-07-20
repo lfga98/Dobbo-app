b266075eb4f8bce9d10b221e8a2e5ecdfc06d85d
lfga98 <lfgarciauaz@gmail.com>
2018-07-20 17:16:02 -0500

    Added the table skeleton for patients

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,7 @@
   Patient.create(first_name:"Leonardo",last_name: "Garcia", maternal_surname: "Acosta",age:10,date_birth: Date.today)
 end
 
-user = User.create(email: "test@test.com",password: "asdfasdf",password_confirmation: "asdfasdf", first_name: "Luis", last_name: "Garcia", maternal_surname: "Acosta",telephone:"1234567890")
+user = User.create(email: "test@test.com",password: "asdfasdf",password_confirmation: "asdfasdf", first_name: "Leo", last_name: "Garcia", maternal_surname: "Acosta",telephone:"1234567890")
 
 user = User.create(email: "user@test.com",password: "asdfasdf",password_confirmation: "asdfasdf", first_name: "Luis", last_name: "Garcia", maternal_surname: "Acosta",telephone:"1234567890")
 
