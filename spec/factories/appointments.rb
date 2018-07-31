db35197d7993bfdf96cbe17190d5a35ed7acf143
lfga98 <lfgarciauaz@gmail.com>
2018-07-31 14:16:02 -0500

    Created the tests for appointments path and new appointment

---
+++ spec/factories/appointments.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :appointment do
+    start_date "5-08-2018 10:00:00"
+    end_date  "5-08-2018 15:00:00"
+    name "Juan Luis Delgado Martinez"
+    user_id=1
+  end
+end
