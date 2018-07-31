b72b8baab95bb660473a095e0d6b80e19849a4ef
lfga98 <lfgarciauaz@gmail.com>
2018-07-30 21:32:45 -0500

    Added tests for appointments, rspec not passing

--- spec/models/appointment_spec.rb
+++ spec/models/appointment_spec.rb
@@ -1,5 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe Appointment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:appointment) do
+      Appointment.create(
+        start_date: Date.today+"10:00:00",
+        end_date: Date.today+"15:00:00",
+        name: "Juanito",
+        patient_id: 1
+        )
+  end
+
+  describe "creation" do
+    it "can be created" do
+      expect(appointment).to be_valid
+    end
+  end
+
+  describe "validations" do
+    it 'has a start date' do
+      appointment.start_date=nil
+      expect(appointment).to_not be_valid
+    end
+    it 'has an end date' do
+      appointment.end_date=nil
+      expect(appointment).to_not be_valid
+    end
+
+
+  end
+
+
+
 end
