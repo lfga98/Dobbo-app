7807e6b339c245ffddd0cfa59a32b55cc04838f9
lfga98 <lfgarciauaz@gmail.com>
2018-08-03 15:51:16 -0500

    Added dairy page and nav icon

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,5 +1,5 @@
 class AppointmentsController < ApplicationController
-  before_action :set_appointment, only: [:show,:edit,:update]
+  before_action :set_appointment, only: [:show,:edit,:update,:destroy]
   protect_from_forgery with: :null_session
 
   def index
@@ -9,7 +9,7 @@ class AppointmentsController < ApplicationController
   def new
     @appointment = Appointment.new
   end
-  
+
   def create
       @appointment= Appointment.new(appointment_params)
       @appointment.user_id=current_user.id
@@ -20,12 +20,17 @@ class AppointmentsController < ApplicationController
       end
   end
 
-  def show
 
+  def destroy
+    @appointment.destroy
+    redirect_to appointments_path, notice: "La cita fue eliminada satisfactoriamente"
+  end
+
+
+  def show
   end
 
   def edit
-
   end
 
   def update
@@ -35,50 +40,6 @@ class AppointmentsController < ApplicationController
       render :edit
     end
   end
-  def data
-   appointments = Appointment.all
-   render :json => appointments.map {|apmt| {
-              :id => apmt.id,
-              :start_date => apmt.start_date.to_formatted_s(:db),
-              :end_date => apmt.end_date.to_formatted_s(:db),
-              :text => apmt.patient.full_name
-          }
-        }
- end
-
- def db_action
-   mode = params["!nativeeditor_status"]
-   id = params["id"]
-   start_date = params["start_date"]
-   end_date = params["end_date"]
-   text = params["observation"]
-
-   case mode
-     when "inserted"
-       apmt = Appointment.create :start_date => start_date, :end_date => end_date, :text => observation,  :patient_id => 1
-
-       tid = apmt.id
-
-     when "deleted"
-       Appointment.find(id).destroy
-       tid = id
-
-     when "updated"
-       apmt = Appointment.find(id)
-       apmt.start_date = start_date
-       apmt.end_date = end_date
-       apmt.text = observation
-       apmt.patient_id = 1
-       apmt.save
-       tid = id
-   end
-
-   render :json => {
-              :type => mode,
-              :sid => id,
-              :tid => tid,
-          }
- end
 
  private
 
