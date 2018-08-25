ba6da5218160cc09cc86a53938b3a4fdde8a1076
lfga98 <lfgarciauaz@gmail.com>
2018-08-24 20:41:49 -0500

    Created the relationshib between medical_history and patient

--- app/models/medical_history.rb
+++ app/models/medical_history.rb
@@ -1,2 +1,4 @@
 class MedicalHistory < ApplicationRecord
+  belongs_to :patient, :class_name =>'Patient', :foreign_key => 'patient_id'
+
 end
--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,9 +1,11 @@
 class Patient < ApplicationRecord
   belongs_to :user,optional: true
+  has_one :medical_history
   validates_presence_of :first_name,:maternal_surname,:telephone,:email
   has_many :appointments, dependent: :delete_all
   validates :telephone, length: { is: 10 }
   validates :telephone, numericality: { only_integer: true }
+
 
   def self.search(search)
     where("first_name ILIKE ? OR id::varchar ILIKE ? OR last_name ILIKE ? or maternal_surname ILIKE ? ",
